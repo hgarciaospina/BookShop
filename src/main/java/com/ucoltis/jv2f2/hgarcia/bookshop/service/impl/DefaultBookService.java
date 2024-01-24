e15e82fcb4294cd62047352ae8ca5f10fd717838
Henry García <henrygarciaospina@gmail.com>
2024-01-23 21:00:34 -0500

    Corrected Create books method.

--- src/main/java/com/ucoltis/jv2f2/hgarcia/bookshop/service/impl/DefaultBookService.java
+++ src/main/java/com/ucoltis/jv2f2/hgarcia/bookshop/service/impl/DefaultBookService.java
@@ -44,40 +44,40 @@ public class DefaultBookService implements BookService {
     private static final String BOOK_PAGES_REQUIRED_CODE = "305";
     private static final String BOOK_PAGES_REQUIRED_MESSAGE = "The number of pages is required";
     
-    private static final String BOOK_RELEASE_DATE_REQUIRED_CODE = "305";
+    private static final String BOOK_RELEASE_DATE_REQUIRED_CODE = "306";
     private static final String BOOK_RELEASE_DATE_REQUIRED_MESSAGE = "The release date is required";
     
-    private static final String BOOK_INVALID_PAGES_CODE = "306";
+    private static final String BOOK_INVALID_PAGES_CODE = "307";
     private static final String BOOK_INVALID_PAGES_MESSAGE = "The number of pages must be greater than zero";
     
-    private static final String BOOK_INFO_REQUIRED_CODE = "307";
+    private static final String BOOK_INFO_REQUIRED_CODE = "308";
     private static final String BOOK_INFO_REQUIRED_MESSAGE = "Book information is required";
 
-    private static final String BOOK_NOT_FOUND_CODE = "308";
+    private static final String BOOK_NOT_FOUND_CODE = "309";
     private static final String BOOK_NOT_FOUND_MESSAGE = "Book not found with the provided id: ";
     
-    private static final String BOOKS_NOT_FOUND_CODE = "309";
+    private static final String BOOKS_NOT_FOUND_CODE = "310";
     private static final String BOOKS_NOT_FOUND_MESSAGE = "Books not found.";
     
-    private static final String BOOK_TITLE_REQUIRED_CODE = "310";
+    private static final String BOOK_TITLE_REQUIRED_CODE = "311";
     private static final String BOOK_TITLE_REQUIRED_MESSAGE = "Book title is required";
 
-    private static final String CATEGORY_INFO_REQUIRED_CODE = "311";
+    private static final String CATEGORY_INFO_REQUIRED_CODE = "312";
     private static final String CATEGORY_INFO_REQUIRED_MESSAGE = "Category information is required";
     
-    private static final String CATEGORY_NOT_FOUND_CODE = "312";
+    private static final String CATEGORY_NOT_FOUND_CODE = "313";
     private static final String CATEGORY_NOT_FOUND_MESSAGE = "Category not found with the provided id: ";
     
-    private static final String CATEGORY_INVALID_ID_CODE ="313";
+    private static final String CATEGORY_INVALID_ID_CODE ="314";
     private static final String CATEGORY_INVALID_ID_MESSAGE = "The category id must be greater than zero and exist in the categories.";
     
-    private static final String AUTHOR_INVALID_ID_CODE = "314"; 
+    private static final String AUTHOR_INVALID_ID_CODE = "315"; 
     private static final String AUTHOR_INVALID_ID_MESSAGE = "The author id must be greater than zero and exist in the authors.";
     
-    private static final String AUTHOR_INFO_REQUIRED_CODE = "315";
+    private static final String AUTHOR_INFO_REQUIRED_CODE = "316";
     private static final String AUTHOR_INFO_REQUIRED_MESSAGE = "Author information is required";
     
-    private static final String AUTHOR_NOT_FOUND_CODE = "316";
+    private static final String AUTHOR_NOT_FOUND_CODE = "317";
     private static final String AUTHOR_NOT_FOUND_MESSAGE = "Author not found with the provided id: ";
 
 	@Override
@@ -110,9 +110,26 @@ public class DefaultBookService implements BookService {
 		if(bookRequest == null ) {
 			throw new BadArgumentException(BOOK_INFO_REQUIRED_CODE, BOOK_INFO_REQUIRED_MESSAGE);		
 		} 
+				
+		var category = categoryRepository.findById(bookRequest.getCategoryId())
+	            .orElseThrow(() -> new NotFoundException(CATEGORY_NOT_FOUND_CODE, CATEGORY_NOT_FOUND_MESSAGE + bookRequest.getCategoryId()));
+		 
+		 var author = authorRepository.findById(bookRequest.getAuthorId())
+		            .orElseThrow(() -> new NotFoundException(AUTHOR_NOT_FOUND_CODE, AUTHOR_NOT_FOUND_MESSAGE + bookRequest.getAuthorId()));
+		 
+		var newBook = new Book();
 		
-		var newBook = modelMapper.map(bookRequest, Book.class); 
-		    newBook = bookRepository.save(newBook);
+		newBook.setTitle(bookRequest.getTitle());
+		newBook.setDescription(bookRequest.getDescription());
+		newBook.setPrice(bookRequest.getPrice());
+		newBook.setIsbn(bookRequest.getIsbn());
+		newBook.setPages(bookRequest.getPages());
+		newBook.setReleaseDate(bookRequest.getReleaseDate());
+		newBook.setImage(bookRequest.getImage());
+	    newBook.setCategory(category);
+	    newBook.setAuthor(author);
+	    
+		newBook = bookRepository.save(newBook);
 		    
 		return modelMapper.map(newBook, BookResponse.class);
 	}
@@ -183,10 +200,10 @@ public class DefaultBookService implements BookService {
 		    	throw new BadArgumentException(AUTHOR_INVALID_ID_CODE, AUTHOR_INVALID_ID_MESSAGE);		
 		 }
 		 
-		 var categoryResponse = categoryRepository.findById(bookRequest.getCategoryId())
+		 var category = categoryRepository.findById(bookRequest.getCategoryId())
 	            .orElseThrow(() -> new NotFoundException(CATEGORY_NOT_FOUND_CODE, CATEGORY_NOT_FOUND_MESSAGE + id));
 		 
-		 var authorResponse = authorRepository.findById(bookRequest.getAuthorId())
+		 var author = authorRepository.findById(bookRequest.getAuthorId())
 		            .orElseThrow(() -> new NotFoundException(AUTHOR_NOT_FOUND_CODE, AUTHOR_NOT_FOUND_MESSAGE + id));
 		 
 		 existingBook.setTitle(bookRequest.getTitle());
@@ -196,8 +213,8 @@ public class DefaultBookService implements BookService {
 		 existingBook.setPages(bookRequest.getPages());
 		 existingBook.setReleaseDate(bookRequest.getReleaseDate());
 		 existingBook.setImage(bookRequest.getImage());
-		 existingBook.setCategory(modelMapper.map(categoryResponse, Category.class));
-		 existingBook.setAuthor(modelMapper.map(authorResponse, Author.class));
+		 existingBook.setCategory(modelMapper.map(category, Category.class));
+		 existingBook.setAuthor(modelMapper.map(author, Author.class));
 		 
 		 bookRepository.save(existingBook);
 		 
