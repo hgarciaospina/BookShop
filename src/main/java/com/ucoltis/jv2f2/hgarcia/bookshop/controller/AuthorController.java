c3e7e5b97be28cb20b8714f6ab8149eda46c4274
Henry García <henrygarciaospina@gmail.com>
2024-02-03 14:58:13 -0500

    Update of the HttpStatus code for the create, or post type methods.

--- src/main/java/com/ucoltis/jv2f2/hgarcia/bookshop/controller/AuthorController.java
+++ src/main/java/com/ucoltis/jv2f2/hgarcia/bookshop/controller/AuthorController.java
@@ -2,6 +2,7 @@ package com.ucoltis.jv2f2.hgarcia.bookshop.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ucoltis.jv2f2.hgarcia.bookshop.controller.dto.AuthorRequest;
@@ -37,6 +39,7 @@ public class AuthorController {
 	}
 	
 	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
 	public AuthorResponse create(@RequestBody AuthorRequest  authorRequest) {
 		return authorService.create(authorRequest);
 		
--- src/main/java/com/ucoltis/jv2f2/hgarcia/bookshop/controller/dto/BookRequest.java
+++ src/main/java/com/ucoltis/jv2f2/hgarcia/bookshop/controller/dto/BookRequest.java
@@ -2,8 +2,6 @@ package com.ucoltis.jv2f2.hgarcia.bookshop.controller.dto;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
